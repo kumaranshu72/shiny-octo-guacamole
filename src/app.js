de2e34d181b00c7d99fceda5049e12ebe2df79ad
kumaranshu72 <kumaranshu72@gmail.com>
2019-03-30 09:31:28 +0530

    body parser added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express'
-var path = require('path')
+
+import path from 'path'
+
+import * as bodyParser from 'body-parser'
 
 const app = express()
 
 // defualt template engine as ejs
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
+
+app.use(bodyParser.urlencoded({ extended: false }))  
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
   res.render('index')

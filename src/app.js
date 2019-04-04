8dc9911c1ba1e148a40073b11cd1e2403834d550
kumaranshu72 <kumaranshu72@gmail.com>
2019-04-04 08:35:50 +0530

    added express session

--- src/app.js
+++ src/app.js
@@ -5,12 +5,24 @@ import * as bodyParser from 'body-parser'
 
 import router from './routes'
 
+const session = require('express-session')
+
 const app = express()
 
 // defualt template engine as ejs
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
 app.use(express.static(path.join(__dirname, '/public')))
+
+app.use(session({
+  key: 'user_id',
+  secret: 'bloody_bitch',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000,
+  },
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,11 +1,21 @@
 import { viewNames } from '../config'
+import { runInNewContext } from 'vm';
 
 export const register = {
   viewForm: (req, res) => {
     res.render(viewNames.REGISTER)
   },
   registerUser: (req, res) => {
-    const { fname, lname, email, mobile, password, ConfirmPassword } = req.body
-    res.send('Hi! We are currently developing this feature')
+    console.log(req.body)
+    if (req.body.fname && req.body.lname && req.body.email && req.body.password && req.body.ConfirmPassword) {
+      const { fname, lname, email, mobile, password, ConfirmPassword } = req.body
+      if (password === ConfirmPassword) {
+        res.send('Hi! We are currently developing this feature')
+      } else {
+        res.send('Password not matched!')
+      }
+    } else {
+      res.send('Fields are missing')
+    }
   },
 }
